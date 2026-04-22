c2f1c10675762d6c6644ff8ff5bc542725ecc281
Kai <khaizafron@gmail.com>
2026-04-22 08:46:46 +0800

    Optimize performance: preload JS bundle, lazy load Elfsight widget to reduce forced reflow and LCP delay

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,23 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), {
-    name: 'preload-css',
+    name: 'preload-assets',
     transformIndexHtml(html) {
-      return html.replace(
+      // Preload CSS
+      html = html.replace(
         /<link rel="stylesheet" crossorigin href="([^"]*\/assets\/index-[^"]*\.css)">/g,
         '<link rel="preload" as="style" href="$1" onload="this.onload=null;this.rel=\'stylesheet\'">'
       )
+      // Preload JS
+      const jsMatch = html.match(/<script type="module" crossorigin src="([^"]*\/assets\/index-[^"]*\.js)">/)
+      if (jsMatch) {
+        const jsUrl = jsMatch[1]
+        html = html.replace(
+          /(<script type="module" crossorigin src="[^"]*\/assets\/index-[^"]*\.js"><\/script>)/,
+          `<link rel="preload" href="${jsUrl}" as="script">\n    $1`
+        )
+      }
+      return html
     }
   }],
   server: {

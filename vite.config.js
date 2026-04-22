596ae0a433f9917e2ae714ba21e9c349384d726a
Kai <khaizafron@gmail.com>
2026-04-22 08:38:51 +0800

    Optimize render-blocking resources: preload CSS and font for better LCP/FCP

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), {
+    name: 'preload-css',
+    transformIndexHtml(html) {
+      return html.replace(
+        /<link rel="stylesheet" crossorigin href="([^"]*\/assets\/index-[^"]*\.css)">/g,
+        '<link rel="preload" as="style" href="$1" onload="this.onload=null;this.rel=\'stylesheet\'">'
+      )
+    }
+  }],
   server: {
     port: 3000,
     host: true
